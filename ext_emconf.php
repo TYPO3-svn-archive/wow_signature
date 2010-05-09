b4127349049d727b13af467bfb3f409bdba5e8c6
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-09 07:26:27 +0000

    unify category & dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wow_signature/trunk@33053 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'World of Warcraft - Signature',
 	'description' => 'Generate signatures for WOW characters',
-	'category' => 'plugin',
+	'category' => 'World of Warcraft',
 	'author' => 'Jobe',
 	'author_email' => 'jobe@jobesoft.de',
 	'shy' => '',
@@ -32,7 +32,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'wow_character' => '',
+			'cms' => '',
+			'typo3' => '4.3.2',
+			'php' => '5.0',
+			'wow_armory' => '0.0.0',
 		),
 		'conflicts' => array(
 		),
